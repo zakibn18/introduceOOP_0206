671fe4f465384cbf0d005ec7c976ee3370b733cc
zakibn18 <jekben815@gmail.com>
2025-04-29 08:50:10 +0700

    second commit

--- bebas.cpp
+++ bebas.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 using namespace std;
 
+class menghitungPersegipanjang
+{
+  public :
+    float panjang, lebar, luas;
 
+    void inputData()
+    {
+      cout << "Masukkan panjang : ";
+      cin >> panjang;
+      cout << "Masukkan lebar : ";
+      cin >> lebar;
+    }
+
+    void menghitungLuas()
+    {
+      luas = panjang * lebar;
+    }
+
+    void displayData()
+    {
+      cout << "Luas persegi panjang adalah " << luas << endl;
+    }
+};
 
 int main()
 {
+  menghitungPersegipanjang per;
+  per.inputData();
+  per.menghitungLuas();
+  per.displayData();
 
+  return 0;
 }
